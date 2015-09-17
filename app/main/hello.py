39e7cb2e4d38d670bacca5e0e4bfd39e74d4d8a4
rebort_yang <353281855@qq.com>
2015-09-17 13:00:09 +0800

    flask : database

--- app/main/hello.py
+++ app/main/hello.py
@@ -11,15 +11,21 @@ from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 from flask import session,redirect,url_for,flash
-
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 template_folder = '../templates'
+baseidr = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__,template_folder=template_folder)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(baseidr,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWM'] = True
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 #路由和view
 @app.route('/',methods=['GET','POST'])
@@ -51,8 +57,27 @@ class NameForms(Form):
     name = StringField('what is your name?',validators=[Required()])
     submit = SubmitField('Submit')
 
+# model
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True,index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
+
     manager.run()
