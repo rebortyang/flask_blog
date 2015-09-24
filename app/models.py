3d4ab369b353b18a7d97edf6e0684f5101d8002b
rebort_yang <353281855@qq.com>
2015-09-24 18:03:19 +0800

    flask :  show user  info page

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from flask.ext.login import UserMixin,AnonymousUserMixin
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from datetime import datetime
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -37,6 +38,11 @@ class User(db.Model,UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(),default = datetime.utcnow)
+    last_seen = db.Column(db.DateTime(),default = datetime.utcnow)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(64),unique=True,index=True)
@@ -119,6 +125,9 @@ class User(db.Model,UserMixin):
         db.session.add(self)
         return True
 
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
 
     def __repr__(self):
@@ -138,6 +147,8 @@ class AnonymousUser(AnonymousUserMixin):
 
     def is_administrator(self):
         return False
+login_manager.anonymous_user = AnonymousUser
+
 
 @login_manager.user_loader
 def load_user(user_id):
